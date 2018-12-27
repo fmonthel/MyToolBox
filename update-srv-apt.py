2b7a64fa81548e2d4e11316b9fe41a3dab5443bb
Florent MONTHEL <fmonthel@flox-arts.net>
2018-12-27 18:41:45 -0500

    Update update-srv-apt.py

--- update-srv-apt.py
+++ update-srv-apt.py
@@ -28,7 +28,7 @@ def update_upgrade_apt(host):
     result = ssh.stdout.readlines()
     if result == []:
         error = ssh.stderr.readlines()
-        logging.getLogger(APPLICATION).error('SSH error raised on "' + str(host) + '" : "' + str(error) + '"')
+        logging.getLogger(APPLICATION).warning('SSH error raised on "' + str(host) + '" : "' + str(error) + '"')
         return -1
     # Return
     return 0
